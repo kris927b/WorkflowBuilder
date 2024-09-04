6f08e396f2347ae7c158fdda510f69b20eb77137
kris927b <kristian.smed.noergaard@gmail.com>
2024-09-04 19:31:15 +0200

    Replacing my own structure with the fastapi fullstack template

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,17 +1,22 @@
-from datetime import datetime, timedelta
-from typing import Any, Union
+from datetime import datetime, timedelta, timezone
+from typing import Any
 
-from jose import jwt
-from fastapi.security import OAuth2PasswordBearer
+import jwt
 from passlib.context import CryptContext
 
 from app.core.config import settings
 
-# Context for hashing passwords
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# OAuth2 scheme for handling token authentication
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+ALGORITHM = "HS256"
+
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
@@ -20,23 +25,3 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
-
-
-def create_access_token(
-    data: dict, expires_delta: Union[timedelta, None] = None
-) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now() + expires_delta
-    else:
-        expire = datetime.now() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
-    )
-    return encoded_jwt
-
-
-def verify_access_token(token: str) -> Any:
-    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-    return payload
--- backend/app/db/base.py
+++
@@ -1,4 +0,0 @@
-from app.db.session import Base
-from app.db.models.user import User
-
-# Import all models here for Alembic to know
